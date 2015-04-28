03c18f3fa8439925e06199ae62a87f07360accbf
Vinay Pasumarthy <venkata.pasumarthy@gmail.com>
2015-04-28 16:06:13 -0700

    Commit with Aggregate and XML Conversions Half Complete

--- src/main/java/com/ultimo/AggregateService.java
+++ src/main/java/com/ultimo/AggregateService.java
@@ -1,15 +1,29 @@
 package com.ultimo;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-
 import io.undertow.server.HttpServerExchange;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.restheart.handlers.PipedHttpHandler;
 import org.restheart.handlers.RequestContext;
 import org.restheart.handlers.RequestContext.METHOD;
 import org.restheart.handlers.applicationlogic.ApplicationLogicHandler;
 import org.restheart.utils.HttpStatus;
 import org.restheart.utils.ResponseHelper;
+import com.mongodb.AggregationOutput;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientException;
+import com.mongodb.util.JSON;
 
 public class AggregateService extends ApplicationLogicHandler {
 
@@ -19,16 +33,88 @@ public class AggregateService extends ApplicationLogicHandler {
 	}
 
 	@Override
-	public void handleRequest(HttpServerExchange exchange,
-			RequestContext context) throws Exception {
-		// TODO Auto-generated method stub
-		 if (context.getMethod() == METHOD.GET) {
-	            ResponseHelper.endExchangeWithMessage(exchange, HttpStatus.SC_OK, "Hello RestHeart");
-	        } else {
-	            exchange.setResponseCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
-	            exchange.endExchange();
-	        }
+	public void handleRequest(HttpServerExchange exchange,RequestContext context) throws Exception {
+		String dbname = context.getDBName();
+		String collectionName = context.getCollectionName();
+
+		MongoClient db = new MongoClient( "172.16.120.70" , 27017 );
+		System.out.println(exchange.getHostName() + " " + exchange.getHostPort());
+        DB database = db.getDB(dbname);
+        DBCollection collection = database.getCollection(collectionName);
+        
+    //============================================================================================================    
+    //  Get Payload  
+        try{
+		InputStream input = exchange.getInputStream();
+		BufferedReader inputReader = new BufferedReader(new InputStreamReader(input));
+		String payload = inputReader.readLine();
+		System.out.println(payload);
+	//===========================================================================================================	
+	// Begin Formatting Query Pipeline	
+		DBObject dbInputObject;
+        List<DBObject> query = new ArrayList<DBObject>();
+        
+        
+		JSONArray inputArray = new JSONArray(payload);
+		for (int a = 0; a < inputArray.length(); a++)
+		{
+			JSONObject inputObject = (JSONObject) inputArray.get(a);
+		if (inputObject.has("$match"))
+        {
+        	System.out.println(inputObject.get("$match").toString());
+        	dbInputObject = new BasicDBObject("$match",JSON.parse(inputObject.get("$match").toString()));
+        	query.add(dbInputObject);
+        	
+        }
+		else if (inputObject.has("$group"))
+		 {
+        	System.out.println(inputObject.get("$group").toString());
+        	dbInputObject = new BasicDBObject("$group",JSON.parse(inputObject.get("$group").toString()));
+        	query.add(dbInputObject);
+        	
+		 }
+		else if (inputObject.has("$sort"))
+		 {
+        	System.out.println(inputObject.get("$sort").toString());
+        	dbInputObject = new BasicDBObject("$sort",JSON.parse(inputObject.get("$sort").toString()));
+        	query.add(dbInputObject);
+        	
+		 }
 		
+        else 
+        {
+        	System.out.println("in Else");
+        }
+		}		
+		System.out.println("Done");
+        
+        
+     //==========================================================================================================
+     // Send Query to DB
+        
+        AggregationOutput output = collection.aggregate( query );
+        JSONArray resultsArray = new JSONArray(output.results().toString());
+        int i = 0;
+        while(i < resultsArray.length())
+        {
+        System.out.println(resultsArray.get(i));
+        	i++;
+        }
+        db.close();
+        
+	}
+        catch(JSONException e) 
+        {
+            ResponseHelper.endExchangeWithMessage(exchange, HttpStatus.SC_NOT_ACCEPTABLE, "Incorrectly Formatted JSON Object. Please check Payload Format");
+            e.printStackTrace();
+
+        }
+        catch(MongoClientException e) 
+        {
+            ResponseHelper.endExchangeWithMessage(exchange, HttpStatus.SC_NOT_FOUND, "Could Not Connect to MongoDB");
+            e.printStackTrace();
+
+        }       
 
 	}
 
--- src/main/java/com/ultimo/XMLConversionServiceOneField.java
+++ src/main/java/com/ultimo/XMLConversionServiceOneField.java
@@ -1,14 +1,33 @@
 package com.ultimo;
+import io.undertow.server.HttpServerExchange;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Map;
+
 import org.json.*;
+import org.restheart.handlers.PipedHttpHandler;
+import org.restheart.handlers.RequestContext;
+import org.restheart.handlers.RequestContext.METHOD;
+import org.restheart.handlers.applicationlogic.ApplicationLogicHandler;
+import org.restheart.utils.HttpStatus;
+import org.restheart.utils.ResponseHelper;
+
 import com.mongodb.*;
 import com.mongodb.util.JSON;
 
-public class XMLConversionServiceOneField 
+public class XMLConversionServiceOneField extends ApplicationLogicHandler
 {
+
+	public XMLConversionServiceOneField(PipedHttpHandler next,
+			Map<String, Object> args) {
+		super(next, args);
+		// TODO Auto-generated constructor stub
+	}
+
 
 	public static String readXML(String file) throws IOException 
 	{
@@ -20,19 +39,20 @@ public class XMLConversionServiceOneField
 		    sb.append(line.trim());
 		}
 		br.close();
-		return sb.toString();
+		return XMLConversion(sb.toString());
 		
 	}
 	
 	
-	public static void XMLConversion(String inputXML) throws UnknownHostException
+	public static String  XMLConversion(String inputXML) throws UnknownHostException
 	{
 		inputXML = inputXML.replace("\"", "&quot");
         String intermediateJSON = "{\"payload\" : \""+ inputXML + "\"}";
         System.out.println(intermediateJSON);
 	    JSONObject jsonObj2 = new JSONObject(intermediateJSON);
 	    System.out.println(jsonObj2.toString());
-        MongoDBDocumentWrite(jsonObj2.toString());
+        //MongoDBDocumentWrite(jsonObj2.toString());
+		return jsonObj2.toString();
 	}
 	
 	public static void MongoDBDocumentWrite(String inputJSON) throws UnknownHostException
@@ -67,18 +87,21 @@ public class XMLConversionServiceOneField
 		}
 	}
 	
-	public static void main(String[] args) throws IOException 
+	@Override
+	public void handleRequest(HttpServerExchange exchange,RequestContext context) throws Exception 
 	{
+				String inputXMLFile = exchange.toString();
+			System.out.println(inputXMLFile.toString());
+		String fileOutput = readXML("C:\\Users\\Vinay\\Desktop\\JavaInput.xml");
+		
+		 if (context.getMethod() == METHOD.GET) {
+	            ResponseHelper.endExchangeWithMessage(exchange, HttpStatus.SC_OK, fileOutput);
+	        } else {
+	            exchange.setResponseCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+	            exchange.endExchange();
+	        }
+		
 
-	/*	String fileOutput = readXML("C:\\Users\\Vinay\\Desktop\\JavaInput.xml");
-		System.out.println(fileOutput);
-		XMLConversion(fileOutput);
-		String outputFromDB = MongoDBDocumentRead("172.16.120.70", 27017, "test", "payloadTest");
-		System.out.println("output from DB:");
-		System.out.println(outputFromDB);
-*/
-		
-		
 	}
 
 }
