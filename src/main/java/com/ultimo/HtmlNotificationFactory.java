93dc371b93ffc9513237a00c2027f43fa758a637
TarunPasumarthi <tyco96@gmail.com>
2015-07-15 16:52:26 -0700

    modified HTMLNotificationFacotry

--- src/main/java/com/ultimo/HtmlNotificationFactory.java
+++ src/main/java/com/ultimo/HtmlNotificationFactory.java
@@ -1,5 +1,11 @@
 package com.ultimo;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.restheart.db.MongoDBClientSingleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,10 +18,31 @@ public class HtmlNotificationFactory {
 		LOGGER.info("Getting the right job for template: "+template);
 		template=template.replaceAll("\\.html", "");	
 		try{
-			NotificationTemplate notificationClass= (NotificationTemplate)Class.forName("com.ultimo."+template).newInstance();
+			// Create a File object on the root of the directory containing the class file
+			File file = new File(MongoDBClientSingleton.getErrorSpotConfig("u-template-location"));
+			    // Convert File to a URL
+			    URL url = file.toURL();          // file:/c:/myclasses/
+			    URL[] urls = new URL[]{url};
+                for(URL url1 : urls){
+                	System.out.println(url1.toString());
+                }
+			    // Create a new class loader with the directory
+			    ClassLoader cl = new URLClassLoader(urls);
+
+			    // Load in the class; MyClass.class should be located in
+			    // the directory file:/c:/myclasses/com/mycompany
+			    Class cls = cl.loadClass(template);
+			
+			NotificationTemplate notificationClass= (NotificationTemplate)cls.newInstance();
 			LOGGER.info("found :"+notificationClass.getClass().toString());
 			return notificationClass;
       	}
+		catch (MalformedURLException e) {
+		
+		}
+		catch (ClassNotFoundException e) {
+			
+		}
 		catch (ClassCastException e){
 			LOGGER.error("the given class: "+template+" does not implement NotificationJob");
 			e.printStackTrace();
