8cd92deb36efe6807d030fb27bbad1a09fc47fe8
Anteneh Benti <anteneh@ultimosoft.com>
2015-08-04 17:08:59 -0700

    Hanled null pointer exception for interface1 and errorType

--- src/main/java/com/ultimo/ReportJob.java
+++ src/main/java/com/ultimo/ReportJob.java
@@ -69,10 +69,10 @@ public class ReportJob implements Job{
 	    	throw e;
 	    }
 	    
-	    if(payload.containsField("interface1") && payload.getString("interface1").length() == 0){
+	    if(payload.containsField("interface1") && payload.getString("interface1") != null && payload.getString("interface1").length() == 0){
 	    	payload.remove("interface1");
 	    }
-	    if(payload.containsField("errorType") && payload.getString("errorType").length() == 0){
+	    if(payload.containsField("errorType") && payload.getString("errorType") != null && payload.getString("errorType").length() == 0){
 	    	payload.remove("errorType");
 	    }
 	    
